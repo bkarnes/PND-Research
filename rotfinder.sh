a5629319de865bd361b22c8474fa365bb1d259c2
Benny Karnes <thedude@blkonline.net>
2024-09-11 19:03:56 +0000

    Add files via upload
    initial upload

---
+++ rotfinder.sh
@@ -0,0 +1,56 @@
+#! /bin/bash
+
+clear
+echo -n "Enter Text: "
+read origtext 
+
+echo
+echo "Rot-1: $(echo $origtext | tr '[A-Za-z]' '[B-ZA-Ab-za-a]')"
+echo
+echo "Rot-2: $(echo $origtext | tr '[A-Za-z]' '[C-ZA-Bc-za-b]')"
+echo
+echo "Rot-3: $(echo $origtext | tr '[A-Za-z]' '[D-ZA-Cd-za-c]')"
+echo
+echo "Rot-4: $(echo $origtext | tr '[A-Za-z]' '[E-ZA-De-za-d]')"
+echo
+echo "Rot-5: $(echo $origtext | tr '[A-Za-z]' '[F-ZA-Ef-za-e]')"
+echo
+echo "Rot-6: $(echo $origtext | tr '[A-Za-z]' '[G-ZA-Fg-za-f]')"
+echo
+echo "Rot-7: $(echo $origtext | tr '[A-Za-z]' '[H-ZA-Gh-za-g]')"
+echo
+echo "Rot-8: $(echo $origtext | tr '[A-Za-z]' '[I-ZA-Hi-za-h]')"
+echo
+echo "Rot-9: $(echo $origtext | tr '[A-Za-z]' '[J-ZA-Ij-za-i]')"
+echo
+echo "Rot-10: $(echo $origtext | tr '[A-Za-z]' '[K-ZA-Jk-za-j]')"
+echo
+echo "Rot-11: $(echo $origtext | tr '[A-Za-z]' '[L-ZA-Kl-za-k]')"
+echo
+echo "Rot-12: $(echo $origtext | tr '[A-Za-z]' '[M-ZA-Lm-za-l]')"
+echo
+echo "Rot-13: $(echo $origtext | tr '[A-Za-z]' '[N-ZA-Mn-za-m]')"
+echo
+echo "Rot-14: $(echo $origtext | tr '[A-Za-z]' '[O-ZA-No-za-n]')"
+echo
+echo "Rot-15: $(echo $origtext | tr '[A-Za-z]' '[P-ZA-Op-za-o]')"
+echo
+echo "Rot-16: $(echo $origtext | tr '[A-Za-z]' '[Q-ZA-Pq-za-p]')"
+echo
+echo "Rot-17: $(echo $origtext | tr '[A-Za-z]' '[R-ZA-Qr-za-q]')"
+echo
+echo "Rot-18: $(echo $origtext | tr '[A-Za-z]' '[S-ZA-Qs-za-r]')"
+echo
+echo "Rot-19: $(echo $origtext | tr '[A-Za-z]' '[T-ZA-St-za-s]')"
+echo
+echo "Rot-20: $(echo $origtext | tr '[A-Za-z]' '[U-ZA-Tu-za-t]')"
+echo
+echo "Rot-21: $(echo $origtext | tr '[A-Za-z]' '[V-ZA-Uv-za-u]')"
+echo
+echo "Rot-22: $(echo $origtext | tr '[A-Za-z]' '[W-ZA-Vw-za-v]')"
+echo
+echo "Rot-23: $(echo $origtext | tr '[A-Za-z]' '[X-ZA-Wx-za-w]')"
+echo
+echo "Rot-24: $(echo $origtext | tr '[A-Za-z]' '[Y-ZA-Xy-za-x]')"
+echo
+echo "Rot-25: $(echo $origtext | tr '[A-Za-z]' '[Z-ZA-Yz-za-y]')"
